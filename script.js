e67164f9bd9508a8c373e7ab3b063eda070aff2c
Eazybel <eazybel27@gmail.com>
2025-08-03 01:53:29 +0300

    Refactor fetch calls in script.js for recipe search functionality

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const input1=document.getElementById("input1")
 const input2=document.getElementById("input2")
 const table=document.getElementById("table")
 
-// find.onclick = async()=>{
-//     const res= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=rice`)
-//     const data=await res.json()
-//     console.log(data)
-//     console.log(input1.vlaue)
+find.onclick = async()=>{
+    const res= await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=111e9b61931c419e856d4712ba13d208`)
+    const data=await res.json()
+    console.log(data)
+    console.log(input1.vlaue)
     
-// }
+}
 
 
 show.onclick = async()=>{
-   const res= await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=sushi&apiKey=111e9b61931c419e856d4712ba13d208`)
+   const res= await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=111e9b61931c419e856d4712ba13d208`)
+   
     const data=await res.json()
-    console.log(data)
+    console.log(data.results)
     // localStorage.clear()
     // for (let i = 0; i < data.meals.length; i++) {
     //     localStorage.setItem(i,data.meals[i].strMeal) 
