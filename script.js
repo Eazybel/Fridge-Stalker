5db7ed88432e724ee13a5aeb592ff2d6d373ce69
Eazybel <eazybel27@gmail.com>
2025-08-03 01:36:43 +0300

    update food.html, index.html and script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const find=document.getElementById("find")
+const show=document.getElementById("show")
+const input1=document.getElementById("input1")
+const input2=document.getElementById("input2")
+const table=document.getElementById("table")
 
-find.onclick = async()=>{
-    const res= await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Pizza")
-    const data=await res.json()
-    console.log(data)
-}
-const show=document.getElementById("find")
+// find.onclick = async()=>{
+//     const res= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=rice`)
+//     const data=await res.json()
+//     console.log(data)
+//     console.log(input1.vlaue)
+    
+// }
+
 
 show.onclick = async()=>{
-    const res= await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Pizza")
+   const res= await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=sushi&apiKey=111e9b61931c419e856d4712ba13d208`)
     const data=await res.json()
     console.log(data)
+    // localStorage.clear()
+    // for (let i = 0; i < data.meals.length; i++) {
+    //     localStorage.setItem(i,data.meals[i].strMeal) 
+    // }
 }
