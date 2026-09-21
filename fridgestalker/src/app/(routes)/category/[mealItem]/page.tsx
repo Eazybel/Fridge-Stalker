d6fd1b0f37f8283f959d9ae3d3610c7865cd657e
Eazybel <eazybel27@gmail.com>
2026-09-21 22:56:58 +0300

    feat: enhance MealItems component with meal details and image rendering

--- fridgestalker/src/app/(routes)/category/[mealItem]/page.tsx
+++ fridgestalker/src/app/(routes)/category/[mealItem]/page.tsx
@@ -1,19 +1,27 @@
 "use client"
 import {useState,useEffect} from 'react'
 import {useRouter,useParams} from "next/navigation"
-
+import Image from "next/image"
+import Link from 'next/link'
+type mealsType={
+  idMeal:string,
+  strArea:string,
+  strCountry:string,
+  strMeal:string,
+  strMealThumb:string
+}
 
 export default function MealItems() {
-  const [meals,setMeals]=useState({})
+  const [meals,setMeals]=useState<mealsType[]|[]>([])
   const [isLoading,setLoad]=useState(true)
   const [error,setError]=useState(null)
   const paramsItem=useParams<{mealItem:string}>()
   useEffect(()=>{
 const fetchMeal=async()=>{
 try{
-const res=await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${paramsItem}`)
+const res=await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${paramsItem?.mealItem}`)
 const data=await res.json()
-console.log(data)
+setMeals(data.meals)
 }catch(error:any){
 setLoad(false)
 setError(error)
@@ -24,6 +32,26 @@ setLoad(false)
 fetchMeal()
   },[])
   return (
-    <div>{paramsItem?.mealItem}</div>
+    <>
+    {
+      meals&&meals.map(meal=>{
+        return <div key={meal.idMeal}>
+
+          <p>{meal.idMeal}</p><p>{meal.strArea}</p>
+          <p>{meal.strCountry}</p>
+          <p>{meal.strMeal}</p>
+          <Image
+          src={`${meal.strMealThumb}`}
+          alt="meal image"
+          width={500}
+          height={500}
+          />
+          {/* <Link
+          href={}
+          /> */}
+        </div>
+      })
+    }
+    </>
   )
 }
---
+++ fridgestalker/src/app/(routes)/meal/[mealType]/page.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+import {useState,useEffect} from 'react'
+import {useRouter,useParams} from "next/navigation"
+export default function page({}: Props) {
+    
+  return (
+    <div>page</div>
+  )
+}
