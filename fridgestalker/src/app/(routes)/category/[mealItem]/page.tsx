3c503ee53c9755b1cf38df1d93fa5ab9669af352
Eazybel <eazybel27@gmail.com>
2026-09-21 20:34:37 +0300

    feat: implement meal fetching logic and loading/error states in MealItems component

--- fridgestalker/src/app/(routes)/category/[mealItem]/page.tsx
+++ fridgestalker/src/app/(routes)/category/[mealItem]/page.tsx
@@ -2,8 +2,27 @@
 import {useState,useEffect} from 'react'
 import {useRouter,useParams} from "next/navigation"
 
+
 export default function MealItems() {
+  const [meals,setMeals]=useState({})
+  const [isLoading,setLoad]=useState(true)
+  const [error,setError]=useState(null)
   const paramsItem=useParams<{mealItem:string}>()
+  useEffect(()=>{
+const fetchMeal=async()=>{
+try{
+const res=await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${paramsItem}`)
+const data=await res.json()
+console.log(data)
+}catch(error:any){
+setLoad(false)
+setError(error)
+}finally{
+setLoad(false)
+}
+}
+fetchMeal()
+  },[])
   return (
     <div>{paramsItem?.mealItem}</div>
   )
