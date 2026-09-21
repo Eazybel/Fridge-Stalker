ecccbc7500c1402a726c4beb117fa570064fb771
Eazybel <eazybel27@gmail.com>
2026-09-21 17:48:07 +0300

    fix: improve error handling and loading state in Home component

--- fridgestalker/src/app/page.tsx
+++ fridgestalker/src/app/page.tsx
@@ -1,25 +1,29 @@
 import {useState,useEffect} from "react"
 
 export default function Home() {
-const [category,setCategory]=useState({})
-const [loading,setLoad]=useState(true)
-const [error,setError]=useState("")
+  const [category,setCategory]=useState({})
+  const [isLoading,setLoad]=useState(true)
+  const [error,setError]=useState({})
   useEffect(()=>{
-    const fetchData=async()=>{
-fetch( `https://www.themealdb.com/api/json/v1/1/categories.php`)
-.then(res=>{
-  return res.json()
-})
-.then(data=>{
+const fetchCategory=async()=>{
+const res=await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+if(!res.ok){
+  setError(res)
+}else{
+const data=await res.json()
 setCategory(data)
 setLoad(false)
-})
-.catch(error=>{
-  setError(error.message)
-})
-    }
+}
+}
   },[])
-  return (<>
-  <p>recipi api</p>
-  </>);
+return(
+
+  <>
+  {isLoading&& <p>Loading</p>}
+  {category&&
+  
+  console.log(category)
+      }
+  </>
+)
 }
