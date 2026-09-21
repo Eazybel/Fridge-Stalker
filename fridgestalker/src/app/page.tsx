15644463bd0fb45a2f23e2343f4ea68179beb309
Eazybel <eazybel27@gmail.com>
2026-09-21 17:37:57 +0300

    feat: implement data fetching for meal categories in Home component

--- fridgestalker/src/app/page.tsx
+++ fridgestalker/src/app/page.tsx
@@ -1,6 +1,24 @@
-import Image from "next/image";
+import {useState,useEffect} from "react"
 
 export default function Home() {
+const [category,setCategory]=useState({})
+const [loading,setLoad]=useState(true)
+const [error,setError]=useState("")
+  useEffect(()=>{
+    const fetchData=async()=>{
+fetch( `https://www.themealdb.com/api/json/v1/1/categories.php`)
+.then(res=>{
+  return res.json()
+})
+.then(data=>{
+setCategory(data)
+setLoad(false)
+})
+.catch(error=>{
+  setError(error.message)
+})
+    }
+  },[])
   return (<>
   <p>recipi api</p>
   </>);
