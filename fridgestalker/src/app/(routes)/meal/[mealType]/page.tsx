be3e97a50074f07a4ea9fc4f75fe4a8f5768a287
Eazybel <eazybel27@gmail.com>
2026-09-22 12:56:58 +0300

    feat: update mealType definition and improve recipe state management

--- fridgestalker/src/app/(routes)/meal/[mealType]/page.tsx
+++ fridgestalker/src/app/(routes)/meal/[mealType]/page.tsx
@@ -5,17 +5,19 @@ type mealType = {
     strCategory:string,
     strCountry:string,
     strMeasure:string,
+    strIngredient:string,
     strInstractions:string,
     strMeal:string,
     strMealThumb:string,
     strYoutube:string
 }
+type mealTypePartia=Partial<mealType>
 
 import {useState,useEffect} from 'react'
 import {useParams} from "next/navigation"
 export default function page() {
     const paramsItem=useParams<{mealType:string}>()
-    const [recipe,setRecipe]=useState<mealType|{}>({})
+    const [recipe,setRecipe]=useState<mealTypePartia|undefined>(undefined)
     const [isLoading,setLoad]=useState(true)
     const [error,setError]=useState(null)
 useEffect(()=>{
@@ -34,7 +36,7 @@ setLoad(false)
 
 }
 fetchData()
-},[])
+},[recipe?.idMeal])
   return (
    <>
     {
@@ -44,9 +46,16 @@ fetchData()
         error&&<p>{error}</p>
     }
     {
-        recipe&&
-        // <p>{recipe.idMeal}</p>
-        console.log(recipe)
+        recipe&& <>
+        <p>{recipe.idMeal}</p>
+        <p>{recipe.strArea}</p>
+        <p>{recipe.strCategory}</p>
+        <p>{recipe.strCountry}</p>
+        <p>{recipe.strMeal}</p>
+        <p>{recipe.strMealThumb}</p>
+        <p>{recipe.strYoutube}</p>
+        </>
+      
 
     }
    
