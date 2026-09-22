0aaa237f56c4f40d10437c204b5dbddfdf399d62
Eazybel <eazybel27@gmail.com>
2026-09-22 15:04:53 +0300

    feat: add video embedding and image display to meal page

--- fridgestalker/src/app/(routes)/meal/[mealType]/page.tsx
+++ fridgestalker/src/app/(routes)/meal/[mealType]/page.tsx
@@ -15,11 +15,13 @@ type mealTypePartia=Partial<mealType>
 // @ts-ignore
 import ReactPlayer from "react-player/lazy"
 import {useState,useEffect} from 'react'
+import Image from "next/image"
 import {useParams} from "next/navigation"
 export default function page() {
     const paramsItem=useParams<{mealType:string}>()
     const [recipe,setRecipe]=useState<mealTypePartia|undefined>(undefined)
     const [isLoading,setLoad]=useState(true)
+    const [url,setUrl]=useState("")
     const [error,setError]=useState(null)
 useEffect(()=>{
 const fetchData=async()=>{
@@ -28,6 +30,11 @@ const res =await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${p
 const data=await res.json() 
 setRecipe(data.meals[0])
 setLoad(false)
+if(recipe?.strYoutube){
+const strUrl=recipe?.strYoutube.split("v=")[1]
+const validUrl=`https://www.youtube.com/embed/${strUrl}`
+setUrl(validUrl)
+}
 } catch (error:any) {
     setError(error)
     setLoad(false)
@@ -47,14 +54,30 @@ fetchData()
         error&&<p>{error}</p>
     }
     {
-        recipe&& <>
+  
+    
+    
+        recipe&&
+        <>
+        
         <p>{recipe.idMeal}</p>
         <p>{recipe.strArea}</p>
         <p>{recipe.strCategory}</p>
         <p>{recipe.strCountry}</p>
         <p>{recipe.strMeal}</p>
-        <p>{recipe.strMealThumb}</p>
-        <iframe src="https://www.youtube.com/embed/9ytR28QK6I8">How to make it?</iframe>
+        <p></p>
+            <Image
+            src={`${recipe.strMealThumb}`}
+            width={500}
+            height={500}
+            alt="food thumbnail"
+            loading="eager"
+            
+            />
+        <iframe
+        width={300}
+        height={300}
+        src={url?`${url}`:undefined}>How to make it?</iframe>
         </>
       
 
